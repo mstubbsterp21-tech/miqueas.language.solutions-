bf5e456213a27c20dfff6560ad52f3fe358aaa47
mstubbsterp21-tech <mstubbsterp21@gmail.com>
2026-04-16 20:30:45 -0400

    Add ScrollToTop component for page reset on navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 
-// Create the root of the React application and render the App component
-// inside a BrowserRouter for client-side routing.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
