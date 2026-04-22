01ef6a30c31616dc861904ed7aa42b8836e7a721
mstubbsterp21-tech <mstubbsterp21@gmail.com>
2026-04-22 15:12:55 -0400

    Remove site assistant from live app shell

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/ScrollToTop';
-import SiteAssistant from './components/SiteAssistant';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <ScrollToTop />
       <App />
-      <SiteAssistant />
     </BrowserRouter>
   </React.StrictMode>,
 );
