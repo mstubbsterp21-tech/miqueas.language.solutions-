3b20b28f5e4fecf7110f77a8d9fd7aafa1164cb3
mstubbsterp21-tech <mstubbsterp21@gmail.com>
2026-04-22 13:58:55 -0400

    Mount site assistant globally

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/ScrollToTop';
+import SiteAssistant from './components/SiteAssistant';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <ScrollToTop />
       <App />
+      <SiteAssistant />
     </BrowserRouter>
   </React.StrictMode>,
 );
