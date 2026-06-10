6e519d322ad46e2f32421329bf325ac53cad549c
mstubbsterp21-tech <mstubbsterp21@gmail.com>
2026-06-10 14:07:09 -0400

    Wrap app with Clerk provider when configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/ScrollToTop';
+import { clerkPublishableKey, isClerkConfigured } from './lib/env';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const app = (
   <React.StrictMode>
     <BrowserRouter>
       <ScrollToTop />
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  isClerkConfigured ? <ClerkProvider publishableKey={clerkPublishableKey}>{app}</ClerkProvider> : app,
 );
