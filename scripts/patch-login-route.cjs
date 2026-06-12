55eaf9c18b512c45180ce7b3837830ad5d523a09
mstubbsterp21-tech <mstubbsterp21@gmail.com>
2026-06-12 11:22:07 -0400

    Add admin interpreter profile route patch

--- scripts/patch-login-route.cjs
+++ scripts/patch-login-route.cjs
@@ -3,11 +3,26 @@ const fs = require('fs');
 const appPath = 'src/App.jsx';
 
 if (fs.existsSync(appPath)) {
-  const source = fs.readFileSync(appPath, 'utf8');
-  const patched = source.replace(
+  let source = fs.readFileSync(appPath, 'utf8');
+
+  source = source.replace(
     '<Route path="/login" element={<Login palette={palette} />} />',
     '<Route path="/login/*" element={<Login palette={palette} />} />'
   );
 
-  fs.writeFileSync(appPath, patched);
+  if (!source.includes('AdminInterpreterProfile')) {
+    source = source.replace(
+      'import AdminInterpreters from "./pages/AdminInterpreters";',
+      'import AdminInterpreters from "./pages/AdminInterpreters";\nimport AdminInterpreterProfile from "./pages/AdminInterpreterProfile";'
+    );
+  }
+
+  if (!source.includes('path="/admin/interpreters/:interpreterId"')) {
+    source = source.replace(
+      '<Route path="/admin/interpreters" element={isClerkConfigured ? <RequirePortalAuth><AdminInterpreters palette={palette} /></RequirePortalAuth> : <PortalSetupNotice palette={palette} />} />',
+      '<Route path="/admin/interpreters" element={isClerkConfigured ? <RequirePortalAuth><AdminInterpreters palette={palette} /></RequirePortalAuth> : <PortalSetupNotice palette={palette} />} />\n          <Route path="/admin/interpreters/:interpreterId" element={isClerkConfigured ? <RequirePortalAuth><AdminInterpreterProfile palette={palette} /></RequirePortalAuth> : <PortalSetupNotice palette={palette} />} />'
+    );
+  }
+
+  fs.writeFileSync(appPath, source);
 }
