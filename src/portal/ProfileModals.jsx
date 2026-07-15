0da9d61d2ade559772faa2efc4f78aa6d6278315
mstubbsterp21-tech <mstubbsterp21@gmail.com>
2026-07-15 15:55:00 -0400

    Show customized profiles in admin account views

--- src/portal/ProfileModals.jsx
+++ src/portal/ProfileModals.jsx
@@ -1,15 +1,20 @@
 import { ClientProfileForm, InterpreterProfileForm } from "./forms";
 import { Modal } from "./ui";
+import ProfileStudio from "./ProfileStudio";
 import { AccountDetail } from "./views";
 import { AssignmentDetail } from "./AssignmentDetail";
 
-export default function ProfileModals({ controller }) {
+export default function ProfileModals({ controller, v2, profileActions }) {
   const {
     modal, setModal, profileType, clientDraft, setClientDraft,
     interpreterDraft, setInterpreterDraft, selectedAssignment,
     accountType, accountRecord, workspace, role, saving, busyDoc,
     actions, saveProfile,
   } = controller;
+  const combinedActions = profileActions || actions;
+  const accountCustomization = (v2?.profileCustomizations || []).find((item) => (
+    accountType === "client" ? item.client_id === accountRecord?.id : item.interpreter_id === accountRecord?.id
+  ));
 
   return (
     <>
@@ -24,7 +29,16 @@ export default function ProfileModals({ controller }) {
       </Modal>
 
       <Modal open={modal === "account"} close={() => setModal("")} title={accountType === "client" ? "Client account" : "Interpreter profile"} wide>
-        <AccountDetail type={accountType} record={accountRecord} workspace={workspace} actions={actions} busyDoc={busyDoc} />
+        <div className="space-y-6">
+          <ProfileStudio
+            profileType={accountType || "interpreter"}
+            profile={accountRecord || {}}
+            customization={accountCustomization}
+            actions={combinedActions}
+            ownerId={accountRecord?.id}
+          />
+          <AccountDetail type={accountType} record={accountRecord} workspace={workspace} actions={actions} busyDoc={busyDoc} />
+        </div>
       </Modal>
     </>
   );
