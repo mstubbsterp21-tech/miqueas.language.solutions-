fcaaf579b8ee1ea5693228429d0e9ed3fee0dbe5
mstubbsterp21-tech <mstubbsterp21@gmail.com>
2026-07-16 08:48:12 -0400

    Add assignment edit and delete toolbar

--- src/portal/AssignmentDetail.jsx
+++ src/portal/AssignmentDetail.jsx
@@ -1,9 +1,11 @@
+import AssignmentAdminControls from "./AssignmentAdminControls";
 import AssignmentDocumentCenter from "./AssignmentDocumentCenter";
 import { AssignmentDetail as CoreAssignmentDetail } from "./views";
 
 export function AssignmentDetail(props) {
   return (
     <div className="space-y-6">
+      {props.role === "admin" && <AssignmentAdminControls assignment={props.assignment} clients={props.clients || []} actions={props.actions} />}
       <CoreAssignmentDetail {...props} />
       <AssignmentDocumentCenter assignment={props.assignment} role={props.role} />
     </div>
